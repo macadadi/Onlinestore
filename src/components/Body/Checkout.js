327fd8e426385745663d2f744fe62eef4972ff33
macadadi <macadadi36@gmail.com>
2021-05-22 23:17:21 +0300

    working on checkout

--- src/components/Body/Checkout.js
+++ src/components/Body/Checkout.js
@@ -4,6 +4,7 @@ function Checkout({title,price}) {
     return (
         <div className='checkout-prop'>
            <h4>{title}</h4> <p>{price}</p>
+           <button className='delete-btn'>X</button>
         </div>
     )
 }
--- src/components/Body/ProductDetails.js
+++ src/components/Body/ProductDetails.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Col, Row } from 'reactstrap'
 import Checkout from './Checkout'
 import './productdetails.css'
 
 function ProductDetails() {
-    const cart = useSelector(state=>state.products.cart)
+    const cart = useSelector(state=>state.products)
+
+
+ 
     return (
         <div className='popup'>
         <div className='box'>
@@ -15,16 +18,44 @@ function ProductDetails() {
                 <Row>
                 <Col  md="6" className='checkout-left'>
                 <h1>Checkout</h1>
-                    {cart && cart.map((product,index)=> <Checkout key={index} title={product.title}  price={product.price}/>)}
+                    {cart.cart && cart.cart.map((product,index)=> <Checkout key={index} title={product.title}  price={product.price}/>)}
                     <div className='checkout-total'>
-                   <h4>TOTAL :</h4> <h5> ksh. 56.00</h5> 
+                   <h4>TOTAL :</h4> <h5> ksh. {cart.total.toFixed(2)}</h5> 
                    </div>
                </Col> 
                <Col  md="6" className='checkout-right'>
-                   <div className='account'>account</div>
-                   <div className='shipping'>shiping</div>
+                   <div className='account'><h3>Credit Card Details</h3>
+                      <div className='card-input'>
+                          <div>
+                              <h5>CARD HOLDER</h5>
+                              <input type='text'  placeholder='Card Holder'/>
+                          </div>
+                          <div>
+                              <h5>EXPIRATION DATE</h5>
+                              <div className='card-date'>
+                              <input type='text'  placeholder='MM'/>
+                              <input type='text'  placeholder='YY'/>
+                              </div>
+                          </div>
+                      </div>
+                      <div className='card-input'>
+                          <div>
+                              <h5>CARD NUMBER</h5>
+                              <input type='text'  placeholder='Card Number'/>
+                          </div>
+                          <div>
+                              <h5 >cvc</h5>
+                              <input className='card-date' type='text'  placeholder='cvc'/>
+                          </div>
+                      </div>
+                   </div>
+                   <div className='shipping'><button className='confirm-btn'>CONFIRM</button></div>
                </Col>
                </Row>
+               <div className='submit-btn-div'>
+               <button className='btn-confirm'>Confirm</button>
+               <button className='btn-cancel' >Cancel</button>
+               </div>
            </div>
         </div>
         </div>
